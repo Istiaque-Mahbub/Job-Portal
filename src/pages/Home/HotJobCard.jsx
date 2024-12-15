c61d1cb4135e3db2afd46ece8ba61ebb60715ed2
Istiaque Mahbub <istiaquemahbub1@gmail.com>
2024-12-15 09:17:49 +0600

    Job details section created

--- src/pages/Home/HotJobCard.jsx
+++ src/pages/Home/HotJobCard.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { CiLocationOn } from "react-icons/ci";
+import { Link } from 'react-router-dom';
 const HotJobCard = ({job}) => {
-    const {title,description,company,company_logo,jobType,salaryRange,requirements,location} = job
+    const {_id,title,description,company,company_logo,jobType,salaryRange,requirements,location} = job
     return (
-        <div className="card card-compact bg-base-100  shadow-xl btn-outline ">
+        <div className="card card-compact bg-base-100  shadow-xl btn-outline">
        <div className='flex gap-2 m-2'>
        <figure>
           <img
@@ -11,15 +12,15 @@ const HotJobCard = ({job}) => {
             src={company_logo}
             alt={`${company} image`} />
         </figure>
-        <div>
+        <div className='mt-4'>
             <h4 className="text-2xl">{company}</h4>
             <p className='flex items-center gap-1 text-gray-600'><CiLocationOn /> {location}</p>
         </div>
        </div>
-        <div className="card-body">
+        <div className="card-body mt-4">
           <h2 className="card-title">{title}</h2>
           <p>{description}</p>
-          <div className='flex flex-wrap gap-2'>
+          <div className='flex flex-wrap gap-2 mt-4'>
             {
                 requirements.map((skill,idx)=><p key={idx}
                 className='border border-blue-600 rounded-lg text-center px-2 hover:text-blue-700 hover:bg-gray-300'
@@ -28,7 +29,9 @@ const HotJobCard = ({job}) => {
           </div>
           <div className="card-actions justify-end items-center mt-4">
             <p>Salary: {salaryRange.min} - {salaryRange.max} {salaryRange.currency}</p>
+            <Link to={`/jobs/${_id}`}>
             <button className="btn btn-primary">Apply</button>
+            </Link>
           </div>
         </div>
       </div>
---
+++ src/pages/JobDetails/JobDetails.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const JobDetails = () => {
+    const {title,description,company,company_logo,jobType,salaryRange,requirements,location,applicationDeadline,hr_name,hr_email,responsibilities,status} = useLoaderData();
+   
+    return (
+        <div className='space-y-4 m-8 px-10'>
+            <h2 className='flex gap-2 items-center'><img src={company_logo} alt={`logo of ${company}`} />Job Details of {company}</h2>
+            <p>Apply for: {title}</p>
+            <p>Job Description: {description}</p>
+            <p className='flex gap-4'> Requirements:
+              {
+                requirements.map((skill,idx)=><p key={idx}
+                className='border  border-blue-600 rounded-lg text-center px-2 hover:text-blue-700 hover:bg-gray-300'
+                > {skill}</p>)
+              }
+            </p>
+            <p className='flex gap-4 items-center'> Responsibilities:
+              {
+                requirements.map((skill,idx)=><p key={idx}
+                className='border  border-blue-600 rounded-lg text-center px-2 hover:text-blue-700 hover:bg-gray-300'
+                > {responsibilities}</p>)
+              }
+            </p>
+            <p>Location: {location}</p>
+            <p>Salary: {salaryRange.min} - {salaryRange.max} {salaryRange.currency}</p>
+            <p>Job Type: {jobType}</p>
+            <p>Dateline: {applicationDeadline}</p>
+            <p>Status: {status}</p>
+            <p>HR Name: {hr_name}</p>
+            <p>HR Email: {hr_email}</p>
+            <button className="btn btn-primary">Apply Now</button>
+        </div>
+    );
+};
+
+export default JobDetails;
